39d17d1f3b7aa80dd260e60a24c7c05f0320de05
mky120799 <mky120799@gmail.com>
2025-07-11 18:37:07 +0530

    implementing auth using passport js and jwt

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,29 +1,11 @@
-const { User } = require("../model/User");
+const express = require("express");
+const { createUser, loginUser, checkUser } = require("../controller/Auth");
+const passport = require("passport");
 
-exports.createUser = async (req, res) => {
-  const user = new User(req.body);
-  try {
-    const doc = await user.save();
-    res.status(201).json({ id: doc.id, role: doc.role });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-};
-
-exports.loginUser = async (req, res) => {
-  try {
-    const user = await User.findOne({ email: req.body.email }).exec();
-    // TODO: this is just temporary, we will use strong password auth
-    console.log({ user });
-    if (!user) {
-      res.status(401).json({ message: "no such user email" });
-    } else if (user.password === req.body.password) {
-      // TODO: We will make addresses independent of login
-      res.status(200).json({ id: user.id, role: user.role });
-    } else {
-      res.status(401).json({ message: "invalid credentials" });
-    }
-  } catch (err) {
-    res.status(400).json(err);
-  }
-};
+const router = express.Router();
+//  /auth is already added in base path
+router
+  .post("/signup", createUser)
+  .post("/login", passport.authenticate("local"), loginUser)
+  .get("/check", passport.authenticate("jwt"), checkUser);
+exports.router = router;
